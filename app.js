57542ccf3d845d660906055b93d61046d47d9800
gingin77 <ginniehench@gmail.com>
2017-11-05 00:57:39 -0400

    Side-bar nav and header features near complete
    Color scheme and fonts selected

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-// function menuOpen() {
-//   document.getElementById('hamburger')
-// }
-//
-//
+let spans = document.getElementsByClassName('hamburger')
+
+function menuOpen () {
+  document.getElementById('side_bar').style.width = '20vw'
+  document.getElementById('main').style.marginLeft = '20vw'
+  for (let i = 0; i < spans.length; i++) {
+    spans[i].style.visibility = 'hidden'
+  }
+}
+
+function menuClose () {
+  document.getElementById('side_bar').style.width = '0'
+  document.getElementById('main').style.marginLeft = '0'
+  for (let i = 0; i < spans.length; i++) {
+    spans[i].style.visibility = 'visible'
+  }
+}
+
+// document.getElementById('nav')
 // let hamburgerMenu =
 // hamburger.addEventListener(click, toggleSideBar())
