a4d829c525cbc242d0a9af4bfa17f93c6f057040
gingin77 <ginniehench@gmail.com>
2017-11-06 14:31:35 -0500

    Create page for resume and assign embed properties

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 let sideBar = document.getElementById('side_bar')
 let main = document.getElementById('main')
 let hamburger = document.getElementById('hamburger')
+let footer = document.getElementById('footer')
+let message = document.getElementById('fixed_message')
 
 function menuOpen () {
   sideBar.setAttribute('style', 'width:180px;')
   // 'max-width: 200px; width:24vw; min-width: 160px;'
   main.style.marginLeft = '180px'
+  footer.style.marginLeft = '180px'
+  message.style.marginLeft = '90px'
   hamburger.style.visibility = 'hidden'
 }
 
 function menuClose () {
   sideBar.setAttribute('style', 'width:0')
   main.style.marginLeft = '0'
+  footer.style.marginLeft = '0'
+  message.style.marginLeft = '0'
   hamburger.style.visibility = 'visible'
 }
 
