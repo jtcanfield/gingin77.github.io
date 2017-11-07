16f6f417b0d62c0faf9fce4e2966632169a8926b
gingin77 <ginniehench@gmail.com>
2017-11-07 18:14:16 -0500

    More content updates and styling edits
    Favicon redesign to something more readable at low res
    Wrote About content and placed it in distinct section, seperate from image_holder
    Fixed issue where internal href links were landing in off position

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ let sideBar = document.getElementById('side_bar')
 let main = document.getElementById('main')
 let hamburger = document.getElementById('hamburger')
 let footer = document.getElementById('footer')
-let message = document.getElementById('fixed_message')
+// let message = document.getElementById('about_centered')
 
 function menuOpen () {
   sideBar.setAttribute('style', 'width:180px;')
   // 'max-width: 200px; width:24vw; min-width: 160px;'
   main.style.marginLeft = '180px'
   footer.style.marginLeft = '180px'
-  message.style.marginLeft = '90px'
+  // message.style.marginLeft = '90px'
   hamburger.style.visibility = 'hidden'
 }
 
@@ -17,7 +17,7 @@ function menuClose () {
   sideBar.setAttribute('style', 'width:0')
   main.style.marginLeft = '0'
   footer.style.marginLeft = '0'
-  message.style.marginLeft = '0'
+  // message.style.marginLeft = '0'
   hamburger.style.visibility = 'visible'
 }
 
