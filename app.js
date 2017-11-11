2d1426e3ef9c53095b071ad8126ca60da9343660
gingin77 <ginniehench@gmail.com>
2017-11-11 13:38:19 -0500

    Resolve error related to removing dl element on mobile
    The function gets called more than once. On second call, the element has already been removed. THerefore a conditional is needed. I used if element is NOT undefined, then remove

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
-//  If media queries, then??
-// Items removed on small devices:
-// From nav bar, no:
-// Home, Connect, logo, check which resume link to keep
 // Remove margin left on sections where it gets added....
-// let lWidth = window.screen.width
 
 window.addEventListener('resize', adjustIfNeeded)
 let viewportWidth = window.innerWidth
 console.log('screen width ' + viewportWidth)
-if (viewportWidth <= 500) {
-  adjustForSmallVP()
-} else {
-  console.log('no adjustment needed')
+
+let loader = document.getElementById('loader')
+let contents = document.getElementById('contents')
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  console.log(state)
+  if (viewportWidth > 500) {
+    console.log('vp bigger than 500')
+    if (state === 'interactive') {
+      loader.classList.add('fadeIn')
+      contents.classList.add('fadeOut')
+    } else if (state === 'complete') {
+      setTimeout(function () {
+        loader.classList.remove('fadeIn')
+        loader.classList.add('fadeOut')
+        loader.remove()
+        showContents()
+      }, 500)
+    }
+  } else {
+    adjustForSmallVP()
+    console.log('vp smallare than 500')
+    loader.remove()
+  }
 }
+
+function showContents () {
+  setTimeout(function () {
+    contents.classList.remove('fadeOut')
+    contents.classList.add('fadeIn')
+  }, 100)
+}
+
+// if (viewportWidth <= 500) {
+//   adjustForSmallVP()
+// } else {
+//   console.log('no adjustment needed')
+// }
 
 function adjustIfNeeded () {
   viewportWidth = window.innerWidth
   if (viewportWidth <= 500) {
+    loader.remove()
     adjustForSmallVP()
   } else {
     console.log('no adjustment needed')
@@ -34,7 +64,10 @@ function modResMob () {
   let resume = document.getElementsByTagName('dt')[0]
   resume.innerHTML = '<a href="public/G.Hench_Resume_Nov.pdf" download>Resume <i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>'
   parent.appendChild(resume)
-  document.getElementsByTagName('dl')[0].remove()
+  let dlEl = document.getElementsByTagName('dl')[0]
+  if (dlEl !== undefined) {
+    dlEl.remove()
+  }
 }
 
 function rmElsForMob () {
@@ -49,45 +82,15 @@ function addFooterStysMob () {
   footerEl.classList.add('footer_mobile')
 }
 
-let loader = document.getElementById('loader')
-let contents = document.getElementById('contents')
-
-document.onreadystatechange = function () {
-  var state = document.readyState
-  console.log(state)
-  if (state === 'interactive') {
-    loader.classList.add('fadeIn')
-    contents.classList.add('fadeOut')
-  } else if (state === 'complete') {
-    setTimeout(function () {
-      document.getElementById('interactive')
-      loader.classList.remove('fadeIn')
-      loader.classList.add('fadeOut')
-      loader.remove()
-      showContents()
-    }, 500)
-  }
-}
-
-function showContents () {
-  setTimeout(function () {
-    contents.classList.remove('fadeOut')
-    contents.classList.add('fadeIn')
-  }, 100)
-}
-
 let sideBar = document.getElementById('side_bar')
 let main = document.getElementById('main')
 let hamburger = document.getElementById('hamburger')
 let footer = document.getElementById('footer')
-// let message = document.getElementById('about_centered')
 
 function menuOpen () {
   sideBar.setAttribute('style', 'width:180px;')
-  // 'max-width: 200px; width:24vw; min-width: 160px;'
   main.style.marginLeft = '180px'
   footer.style.marginLeft = '180px'
-  // message.style.marginLeft = '90px'
   hamburger.style.visibility = 'hidden'
 }
 
@@ -95,15 +98,5 @@ function menuClose () {
   sideBar.setAttribute('style', 'width:0')
   main.style.marginLeft = '0'
   footer.style.marginLeft = '0'
-  // message.style.marginLeft = '0'
   hamburger.style.visibility = 'visible'
 }
-
-
-// for (let i = 0; i < spans.length; i++) {
-//   spans[i].style.visibility = 'hidden'
-// }
-
-// for (let i = 0; i < spans.length; i++) {
-//   spans[i].style.visibility = 'visible'
-// }
